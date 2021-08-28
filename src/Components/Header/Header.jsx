d494856babc503e935bab1e86c7557426e8f938f
Dzmitry Harhun <dimagorga@gmail.com>
2021-08-28 11:43:11 +0300

    icons

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import Call from "@material-ui/icons/Call";
+import Mail from "@material-ui/icons/Mail";
 import Menu from "@material-ui/icons/Menu";
 import Navigation from "../Navigation/Navigation";
 import s from "./Header.module.css";
@@ -20,6 +22,7 @@ class Header extends Component {
         <a className={s.logo} href="/">
           Performance
         </a>
+        <p className={s.logoDescr}>Центр творчества</p>
         <div className={s.mobileMenu}>
           <button className={s.burger} onClick={this.toggle}>
             <Menu className={s.burgerIcon} />
@@ -44,13 +47,18 @@ class Header extends Component {
                 </li>
                 <ul className={s.contactList}>
                   Контакты:
-                  <li>
-                    <a className={s.mobileListLink} href="/" tel="/">
+                  <li className={s.mobileListLink}>
+                    <a className={s.mobileListLink} href="tel:+375339044424">
+                      <Call fontSize="inherit" className={s.callIcon} />
                       +375 33 904 44 24
                     </a>
                   </li>
-                  <li>
-                    <a className={s.mobileListLink} href="/" mail="/">
+                  <li className={s.mobileListLink}>
+                    <a
+                      className={s.mobileListLink}
+                      href="mailto:performance_brest@gmail.com"
+                    >
+                      <Mail fontSize="inherit" className={s.callIcon} />
                       performance_brest@gmail.com
                     </a>
                   </li>
--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,3 +1,5 @@
+import Call from "@material-ui/icons/Call";
+import Mail from "@material-ui/icons/Mail";
 import s from "./Navigation.module.css";
 function Navigation() {
   return (
@@ -13,16 +15,28 @@ function Navigation() {
             Преподаватели
           </a>
         </li>
-        <li className={s.menuItem}>
+        {/* <li className={s.menuItem}>
           <a className={s.navLink} href="/">
             Галерея
           </a>
-        </li>
-        <li className={s.menuItem}>
-          <a className={s.navLink} href="/">
-            Контакты
-          </a>
-        </li>
+        </li> */}
+        <ul className={s.contacts}>
+          <li className={s.contactsListItem}>
+            <a className={s.contactsListLink} href="tel:+375339044424">
+              <Call fontSize="inherit" className={s.callIcon} />
+              +375 33 904 44 24
+            </a>
+          </li>
+          <li className={s.contactsListItem}>
+            <a
+              className={s.contactsListLink}
+              href="mailto:performance_brest@gmail.com"
+            >
+              <Mail fontSize="inherit" className={s.callIcon} />
+              performance_brest
+            </a>
+          </li>
+        </ul>
       </ul>
     </nav>
   );
